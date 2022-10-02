aeb5b88576bc5be5446e1fa54a9f5468eab57f3a
Lisias T <me@lisias.net>
2022-10-02 09:41:34 -0300

    Adjustments on README

--- Feature/Kickstarter.py
+++ Feature/Kickstarter.py
@@ -7,7 +7,7 @@ Created on Aug 16, 2022
 __KICKSTARTER_PARTS = [
 		147, 148, 149, 150, 151, 152, 153, 154, 155, 156,
 		157, 158, 159, 161, 162, 163, 164, 165, 166, 169,
-		172, 175, 176, 177,	181
+		172, 175, 176, 177, 181
 	]
 
 def set_kickstarters_1(savegame, selected_hotbar):
